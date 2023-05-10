ba3a092c9fba022c89ed70683250cb0baeb73e54
Timmy Ly <timmy_ly@hotmail.de>
2023-05-10 21:37:06 +0200

    wip

--- VillageAndWells.py
+++ VillageAndWells.py
@@ -23,14 +23,27 @@ import numpy as np
 
 
 
+def reset_Coords(n,m):
+    return np.ones( (2,n*m), dtype=int )*(-1)
 
-
+class SetOfSquares:
+# A set of squares that share the TravelledDistance
+    def __init__(self, n, m, TravelledDistance) -> None:
+        self.n, self.m = n, m
+        self.Coords = reset_Coords(n,m) 
+        self.TravelledDistance = self.set_TravelledDistance(TravelledDistance)
+        self.Amount = 0
+    def set_TravelledDistance(self, TravelledDistance):
+        self.TravelledDistance = TravelledDistance
+    def set_Size(self, Amount):
+        self.Amount = Amount
 class Solution:
     def __init__(self) -> None:
         self.n = 0
         self.m = 0
         self.c = None
         self.TravelledDistance = 0 # current travelleddistance
+        self.AmountOfWells = 0
 
     def set_n(self,n):
         self.n = n
@@ -39,9 +52,8 @@ class Solution:
     def set_VillageMap(self, VillageMap):
         # I prefer numpy
         self.VillageMap = np.array(VillageMap)
-    def InitializeTravelDistance(self):
+    def InitializeTravelDistances(self):
         self.TravelDistances = np.ones((self.n, self.m), dtype=int)*(-1)
-
     # we realize that going back and forth starting from a well is the same as starting from a house
     # suppose we find a shortest route between a house and a well, then the corresponding sections of the route are also the shortest routes for houses along the route
     # we further realize that we can find all possible routes of length n by travelling from the current location in all 4 directions. Of course travelling to already visited positions(including the previous one) is useless. What about the case when two people want to move on to the same square? It does not matter, since both have travelled the same distance. We save the TravelledDistance in a separate matrix. We can even create a PreviousLocation matrix from which we can recursively obtain a shortest route. 
@@ -51,30 +63,30 @@ class Solution:
         self.set_n(n)
         self.set_m(m)
         self.set_VillageMap(c)
-        self.InitializeTravelDistance()
-        self.Positions = self.ResetPositions() # Counter for how many squares are occupied right now
-        NumberOfWells = self.FindWells()
-        self.nPositions = NumberOfWells # Number of currently occupied squares
-        while self.nPositions>0:
-            # print("npositions: ", self.nPositions)
+        self.InitializeTravelDistances()
+        CurrentSquares = SetOfSquares(n,m, 0)
+        self.get_Wells() #maybe incorporate get_wells in SetOfSquares? no, dont, you want to be able to get Wells from anywhere
+        CurrentSquares.Amount = self.AmountOfWells
+        while CurrentSquares.Amount>0:
+            # print("npositions: ", self.NumberCurrentlyOccupiedSquares)
             self.TravelledDistance += 2
-            self.Travel()
+            self.Travel(CurrentSquares)
             self.SetOpenProhibitedDistances()
-            # print("Positions\n", self.Positions)
+            # print("SetOfSquares\n", self.SetOfSquares)
             # print("TravelDistance after: \n", self.TravelDistances)
         self.CleanUp()
         return self.TravelDistances
 
 
 
-    def Travel(self):
-        NewPositions = self.ResetPositions()
-        # nDummyCurrentPositions = self.nPositions # Counter for how many squares are occupied right now
+    def Travel(self, CurrentSquares):
+        NewPositions = reset_Coords(self.n,self.m)
+        # nDummyCurrentPositions = self.NumberCurrentlyOccupiedSquares # Counter for how many squares are occupied right now
         nNewPositions = 0
         # print("coordinates before travelling")
-        for i in range(self.nPositions):
-            # print(self.Positions[:,i])
-            LocalPositions, nLocalPositions = self.TravelLocal(self.Positions[:,i])
+        for i in range(CurrentSquares.Amount):
+            # print(self.SetOfSquares[:,i])
+            LocalPositions, nLocalPositions = self.TravelLocal(CurrentSquares.Coords[:,i])
             # print("nlocalpositions: ", nLocalPositions)
             for j in range(nLocalPositions):
                 NewPositions[0][nNewPositions + j] = LocalPositions[0][j]
@@ -82,8 +94,8 @@ class Solution:
             # print(NewPositions)
             # self.UpdateTravelDistanceLocal(nLocalPositions, LocalPositions)
             nNewPositions += nLocalPositions
-        self.Positions = NewPositions
-        self.nPositions = nNewPositions
+        CurrentSquares.Coords = NewPositions
+        CurrentSquares.Amount = nNewPositions
     def TravelLocal(self, CurrentPosition):
         row, col = CurrentPosition
         nNewPositions = 0
@@ -141,8 +153,7 @@ class Solution:
         # need a filter to avoid looping through the whole array again?
         self.FindOpenProhibited()
 
-    def ResetPositions(self):
-        return np.ones( (2,self.n*self.m), dtype=int )*(-1)
+
 
     def FindOpenProhibited(self):
         Mask = ( ((self.VillageMap == "N") | (self.VillageMap == ".")) & (self.TravelDistances == -1 ) )
@@ -154,22 +165,23 @@ class Solution:
             # set TravelDistances to 0 for N and .
             self.TravelDistances[coords] = 0
     # first, get the indices of the wells
-    def FindWells(self):
+    def get_Wells(self):
         Mask = (self.VillageMap == "W")
+        Coords = {}
         rows, cols = np.indices((self.n,self.m))
         rows, cols = rows[Mask], cols[Mask]
-        NumberOfWells = len(rows)
+        self.AmountOfWells = len(rows)
         # Traveldistance starting from well is 0, occupy the Wells
-        for i in range(NumberOfWells):
-            coords = rows[i], cols[i]
-            self.TravelDistances[coords] = 0
-            self.Positions[0][i] = rows[i]
-            self.Positions[1][i] = cols[i]
-        return NumberOfWells
+        for i in range(self.AmountOfWells):
+            CoordsSingle = rows[i], cols[i]
+            self.TravelDistances[CoordsSingle] = 0
+            Coords.add(CoordsSingle)
+        return Coords#############################################Last
+
     def SetOpenProhibitedDistances(self):
         # as per challenge, Traveldistance from N or . are defined as 0. Not sure if I agree. Could detach this part...
-        for i in range(self.nPositions):
-            coords = tuple(self.Positions[:,i])
+        for i in range(self.NumberCurrentlyOccupiedSquares):
+            coords = tuple(self.SetOfSquares[:,i])
             # print("coords", coords)
             # print(self.VillageMap[CurrentPosition])
             if( (self.VillageMap[coords] == 'N') or (self.VillageMap[coords] == '.') ):
