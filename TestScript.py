ff8b9e84362001d80d8af0b1c73ed62264765521
Phong-Minh Timmy Ly <timmy.ly@wwu.de>
2023-06-05 20:14:24 +0200

    works

--- TestScript.py
+++ TestScript.py
@@ -17,14 +17,14 @@ sol.setPrerequisiteAttributes(Ny, Nx, VillageMap)
 if __name__=="__main__":
     # main
     # sol.set_PrerequisiteAttributes()
-    # Unittest.profileCPUUsageOfFunction(sol.chefAndWells, Ny, Nx, VillageMap)
+    Unittest.profileCPUUsageOfFunction(sol.chefAndWells, Ny, Nx, VillageMap)
 
     # get_WellCoordinates
     # Unittest.testgetWellSquares(sol)
     # Unittest.testUpdateVisitedSquares(sol)
-    a = np.arange(10)
-    a = np.reshape(a, (5,2))
-    b = Unittest.testGeneralFunction(convert2DArrayToSetOfTuples, a)
-    c = Unittest.testGeneralFunction(convertSetOfTuplesTo2DArray, b)
+    # a = np.arange(10)
+    # a = np.reshape(a, (5,2))
+    # b = Unittest.testGeneralFunction(convert2DArrayToSetOfTuples, a)
+    # c = Unittest.testGeneralFunction(convertSetOfTuplesTo2DArray, b)
 
 # 
--- VillageAndWells.py
+++ VillageAndWells.py
@@ -3,6 +3,11 @@
 # . open ground 1
 # N prohibited 0
 
+# we realize that going back and forth starting from a well is the same as starting from a house
+# suppose we find a shortest route between a house and a well, then the corresponding sections of the route are also the shortest routes for houses along the route
+# we further realize that we can find all possible routes of length Ny by travelling from the current location in all 4 directions. Of course travelling to already visited positions(including the previous one) is useless. What about the case when two people want to move on to the same square? It does not matter, since both have travelled the same distance. We save the CurrentTravelDistance in a separate matrix. We can even create a PreviousLocation matrix from which we can recursively obtain a shortest route. 
+# It would be great if we could visualize the progression of CurrentTravelDistance
+
 # need to optimize
 # for optimization, we realize that only the first TravelStep allows traveling in all directions
 # afterwards the pointy ends allow 3 directions while the side branches only allow 2-3 directions
@@ -32,14 +37,14 @@ def reset_Coords(Ny,Nx):
     return np.ones( (2,Ny*Nx), dtype=int )*(-1)
 
 # class SetOfSquares:
-# # A set of squares that share the TravelledDistance
-#     def __init__(self, Ny, Nx, TravelledDistance) -> None:
+# # A set of squares that share the CurrentTravelDistance
+#     def __init__(self, Ny, Nx, CurrentTravelDistance) -> None:
 #         self.Ny, self.Nx = Ny, Nx
 #         self.Coords = reset_Coords(Ny,Nx) 
-#         self.TravelledDistance = self.set_TravelledDistance(TravelledDistance)
+#         self.CurrentTravelDistance = self.set_CurrentTravelDistance(CurrentTravelDistance)
 #         self.Amount = 0
-#     def set_TravelledDistance(self, TravelledDistance):
-#         self.TravelledDistance = TravelledDistance
+#     def set_CurrentTravelDistance(self, CurrentTravelDistance):
+#         self.CurrentTravelDistance = CurrentTravelDistance
 #     def set_Size(self, Amount):
 #         self.Amount = Amount
 class Solution:
@@ -48,7 +53,7 @@ class Solution:
         self.Ny = 0
         self.Nx = 0
         self.c = None
-        self.TravelledDistance = 0 # current travelleddistance
+        self.CurrentTravelDistance = 0 # current CurrentTravelDistance
         self.AmountOfWells = 0
         self.VisitedSquares = set()
     def chefAndWells(self, Ny : int, Nx : int, VillageMap : List[List[str]]) -> List[List[int]]:
@@ -58,9 +63,9 @@ class Solution:
         self.initializeCurrentSquares()
         self.updateVisitedSquares(self.CurrentSquares)
         self.addProhibitedToVisitedSquares()
-
         self.calculateTravelDistances()
         # self.CleanUp()
+        print(self.TravelDistances)
         return self.TravelDistances
     def setPrerequisiteAttributes(self, Ny, Nx, VillageMap):
         self.setNy(Ny)
@@ -96,27 +101,25 @@ class Solution:
         self.updateVisitedSquares(ProhibitedSquares)
     def calculateTravelDistances(self):
         """main algorithm"""
-        i = 0
-        while i<1:
+        while self.CurrentSquares:
+            self.CurrentTravelDistance += 2
             self.travel()
-            # self.TravelledDistance += 2
+            self.updateTravelDistances()
             # self.Travel(CurrentSquares)
             # self.SetOpenProhibitedDistances()
             self.updateVisitedSquares(self.CurrentSquares)
-            i +=1
         self.setNonHouseDistances()
     def updateVisitedSquares(self, Squares):
         """add Squares to the VisitedSquares"""
         self.VisitedSquares.update(Squares)
+    # think about checking VisitedSquares after all NewSquares have been created?
     def travel(self):
-        PreviousSquares = self.CurrentSquares
-        self.clearCurrentSquares()
+        """Advance Branches in all directions starting from CurrentSquares"""
+        PreviousSquares = self.CurrentSquares.copy()
+        self.CurrentSquares.clear()
         for Square in PreviousSquares:
             self.travelLocally(Square)
-        self.updateTravelledDistances()
         
-    def clearCurrentSquares(self):
-        self.CurrentSquares = set()
     def travelLocally(self, Square):
         self.travelRight(Square)
         self.travelLeft(Square)
@@ -124,20 +127,20 @@ class Solution:
         self.travelDown(Square)
         # need to think about boundary check and VisitedSquares check?
     def travelRight(self, Square): 
-        NewSquare = np.add(Square, (0,1))
-        if( (NewSquare not in self.VisitedSquares) or (not self.isOutOfRightBoundary(NewSquare)) ):
+        NewSquare = tuple(np.add(Square, (0,1)))
+        if( (NewSquare not in self.VisitedSquares) and (not self.isOutOfRightBoundary(NewSquare)) ):
             self.CurrentSquares.add(NewSquare)
     def travelLeft(self, Square): 
-        NewSquare = np.add(Square, (0,-1))
-        if( (NewSquare not in self.VisitedSquares) or (not self.isOutOfLeftBoundary(NewSquare)) ):
+        NewSquare = tuple(np.add(Square, (0,-1)))
+        if( (NewSquare not in self.VisitedSquares) and (not self.isOutOfLeftBoundary(NewSquare)) ):
             self.CurrentSquares.add(NewSquare)
     def travelUp(self, Square): 
-        NewSquare = np.add(Square, (-1,0))
-        if( (NewSquare not in self.VisitedSquares) or (not self.isOutOfTopBoundary(NewSquare)) ):
+        NewSquare = tuple(np.add(Square, (-1,0)))
+        if( (NewSquare not in self.VisitedSquares) and (not self.isOutOfTopBoundary(NewSquare)) ):
             self.CurrentSquares.add(NewSquare)
     def travelDown(self, Square): 
-        NewSquare = np.add(Square, (1,0))
-        if( (NewSquare not in self.VisitedSquares) or (not self.isOutOfBottomBoundary(NewSquare)) ):
+        NewSquare = tuple(np.add(Square, (1,0)))
+        if( (NewSquare not in self.VisitedSquares) and (not self.isOutOfBottomBoundary(NewSquare)) ):
             self.CurrentSquares.add(NewSquare)
     def isOutOfRightBoundary(self, Square):
         return Square[1]>=self.Ny
@@ -147,141 +150,18 @@ class Solution:
         return Square[0]<0
     def isOutOfBottomBoundary(self, Square):
         return Square[0]>=self.Nx
-    def updateTravelledDistances(self):
+    def updateTravelDistances(self):
         for Square in self.CurrentSquares:
-            pass
-
+            self.TravelDistances[Square] = self.CurrentTravelDistance
     def setNonHouseDistances(self):
         Mask = ((self.VillageMap == "W") | (self.VillageMap == "N") | (self.VillageMap == "."))
         self.TravelDistances = np.where(Mask, 0, self.TravelDistances)
 
 
-        
-
-        
-
-    
-
-
-    # we realize that going back and forth starting from a well is the same as starting from a house
-    # suppose we find a shortest route between a house and a well, then the corresponding sections of the route are also the shortest routes for houses along the route
-    # we further realize that we can find all possible routes of length Ny by travelling from the current location in all 4 directions. Of course travelling to already visited positions(including the previous one) is useless. What about the case when two people want to move on to the same square? It does not matter, since both have travelled the same distance. We save the TravelledDistance in a separate matrix. We can even create a PreviousLocation matrix from which we can recursively obtain a shortest route. 
-    # It would be great if we could visualize the progression of TravelledDistance
 
 
 
 
-
-    def Travel(self, CurrentSquares):
-        NewPositions = reset_Coords(self.Ny,self.Nx)
-        # nDummyCurrentPositions = self.NumberCurrentlyOccupiedSquares # Counter for how many squares are occupied right now
-        nNewPositions = 0
-        # print("coordinates before travelling")
-        for i in range(CurrentSquares.Amount):
-            # print(self.SetOfSquares[:,i])
-            LocalPositions, nLocalPositions = self.TravelLocal(CurrentSquares.Coords[:,i])
-            # print("nlocalpositions: ", nLocalPositions)
-            for j in range(nLocalPositions):
-                NewPositions[0][nNewPositions + j] = LocalPositions[0][j]
-                NewPositions[1][nNewPositions + j] = LocalPositions[1][j]
-            # print(NewPositions)
-            # self.UpdateTravelDistanceLocal(nLocalPositions, LocalPositions)
-            nNewPositions += nLocalPositions
-        CurrentSquares.Coords = NewPositions
-        CurrentSquares.Amount = nNewPositions
-    def TravelLocal(self, CurrentPosition):
-        row, col = CurrentPosition
-        nNewPositions = 0
-        NewPositions = np.ones( (2,4), dtype=int )*(-1)
-        # go right
-        if(col < self.Nx-1):
-            NewCol = col + 1
-            if(self.CheckValid(row, NewCol)):
-                NewPositions[0, nNewPositions] = row
-                NewPositions[1, nNewPositions] = NewCol
-                nNewPositions += 1
-        # go left
-        if(col > 0):
-            NewCol = col - 1
-            if(self.CheckValid(row, NewCol)):
-                NewPositions[0, nNewPositions] = row
-                NewPositions[1, nNewPositions] = NewCol
-                nNewPositions += 1
-        # go up
-        if(row > 0):
-            NewRow = row - 1
-            if(self.CheckValid(NewRow, col)):
-                NewPositions[0, nNewPositions] = NewRow
-                NewPositions[1, nNewPositions] = col
-                nNewPositions += 1
-        # go down
-        if(row < self.Ny-1):
-            NewRow = row + 1
-            if(self.CheckValid(NewRow, col)):
-                NewPositions[0, nNewPositions] = NewRow
-                NewPositions[1, nNewPositions] = col
-                nNewPositions += 1
-        return NewPositions, nNewPositions
-    def CheckValid(self, *coords):
-        # >0 or >-1 are both fine at first since there should be no secondary Travel from a well. Meaning, We travel from all wells. Once a route funnels into another well (which is only possible if two wells are side by side), there is no need to travel further since any other route is longer
-        # >0 ensures no conflict(unwanted travelstop) when setting "." and "N" to 0. 
-        if(self.TravelDistances[coords]>-1):
-            return False
-        elif(self.VillageMap[coords] == "N"):
-            self.TravelDistances[coords]=0
-            return False
-        else:
-            self.UpdateTravelDistance(*coords)
-            return True
-    def UpdateTravelDistanceLocal(self, nLocalPositions, LocalPositions):
-        # print("TravelDistance before: ", self.TravelDistances)
-        for i in range(nLocalPositions):
-            self.TravelDistances[tuple(LocalPositions[:,i])] = self.TravelledDistance
-        # print("TravelDistance after: ", self.TravelDistances)
-    def UpdateTravelDistance(self, *coords):
-        self.TravelDistances[coords] = self.TravelledDistance
-
-    def CleanUp(self):
-        # assign TravelDistances 0 to all remaining N and .
-        # need a filter to avoid looping through the whole array again?
-        self.FindOpenProhibited()
-
-
-
-    def FindOpenProhibited(self):
-        Mask = ( ((self.VillageMap == "N") | (self.VillageMap == ".")) & (self.TravelDistances == -1 ) )
-        rows, cols = np.indices((self.Ny,self.Nx))
-        rows, cols = rows[Mask], cols[Mask]
-        NSquares = len(rows)
-        for i in range(NSquares):
-            coords = rows[i], cols[i]
-            # set TravelDistances to 0 for N and .
-            self.TravelDistances[coords] = 0
-    # first, get the indices of the wells
-
-    def get_Wells_old(self):
-        Mask = (self.VillageMap == "W")
-        Coords = {}
-        rows, cols = np.indices((self.Ny,self.Nx))
-        rows, cols = rows[Mask], cols[Mask]
-        self.AmountOfWells = len(rows)
-        # Traveldistance starting from well is 0, occupy the Wells
-        for i in range(self.AmountOfWells):
-            CoordsSingle = rows[i], cols[i]
-            self.TravelDistances[CoordsSingle] = 0
-            Coords.add(CoordsSingle)
-        return Coords#############################################Last
-
-    def SetOpenProhibitedDistances(self):
-        # as per challenge, Traveldistance from N or . are defined as 0. Not sure if I agree. Could detach this part...
-        for i in range(self.NumberCurrentlyOccupiedSquares):
-            coords = tuple(self.SetOfSquares[:,i])
-            # print("coords", coords)
-            # print(self.VillageMap[CurrentPosition])
-            if( (self.VillageMap[coords] == 'N') or (self.VillageMap[coords] == '.') ):
-                # print("its N or .")
-                # print(coords, "setting Traveldistances to 0")
-                self.TravelDistances[coords] = 0
 
 
 
